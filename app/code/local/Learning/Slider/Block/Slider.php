f2ca40d0ad4370c9eb47005e0edad36c07d284a9
benjamin rouville <benjamin rouville>
2017-05-10 10:01:16 +0200

    intitialize project

---
+++ app/code/local/Learning/Slider/Block/Slider.php
@@ -0,0 +1,18 @@
+<?php
+
+class Learning_Slider_Block_Slider extends Mage_Core_Block_Template
+{
+	/*public function methodblock()
+	{
+    		return 'this is a method from the block !' ;
+	}*/
+  public function getSlides()
+  {
+  	$slides = Mage::getModel('learning_slider/slide')
+  		->getCollection()
+  		->addIsActiveFilter()
+  		->addOrderByPosition();
+
+  	return $slides;
+  }
+}
